fa0d385d40d69ec9ebc6fb5fef2e12ef5ebc77ec
Kels <kelsey.baker1960@gmail.com>
2024-05-08 22:07:26 +0100

    template header complete

--- components/header/header.jsx
+++ components/header/header.jsx
@@ -1,11 +1,46 @@
 "use client"
+import React, { useState} from 'react';
 import styles from './header.module.css'
 import Link from 'next/link';
 
 export default function Header() {
 
+const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
-   <p>HEADER TESTER</p>
+    <header className={styles.header}>
+       <nav className={styles.navbar} >
+        <a href="#" className={styles.navbranding}>Logo</a>
+
+        <ul className={`${styles.navmenu} ${isMenuOpen ? styles.active : ''}`}>
+          <li className={styles.navitem}>
+            <a href="#" className={styles.navlink}>Shop</a>
+          </li>
+          <li className={styles.navitem}>
+            <a href="#" className={styles.navlink}>About</a>
+          </li>
+          <li className={styles.navitem}>
+            <a href="#" className={styles.navlink}>Work</a>
+          </li>
+          <li className={styles.navitem}>
+            <a href="#" className={styles.navlink}>Contact</a>
+          </li>
+          <li className={styles.navitem}>
+            <a href="#" className={styles.navlink}>Socials</a>
+          </li>
+          <li className={styles.navitem}>
+            <a href="#" className={styles.navlink}>Cart</a>
+          </li>
+        </ul>
+        <div className={`${styles.hamburger} ${isMenuOpen ? styles.active : ''}`}
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+        >
+          <span className={styles.bar}></span>
+          <span className={styles.bar}></span>
+          <span className={styles.bar}></span>
+        </div>
+      </nav>
+    </header>
   );
 }
 
