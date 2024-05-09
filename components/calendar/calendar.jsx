ec5c57e793043aea9022afb04d59b9b185526348
Albert-S2 <albert733844@gmail.com>
2024-05-09 11:58:51 +0100

    height added to calendar

--- components/calendar/calendar.jsx
+++ components/calendar/calendar.jsx
@@ -1,17 +1,25 @@
-'use client'
+"use client";
 import React from "react";
 import Fullcalendar from "@fullcalendar/react";
-import timeGridPlugin from '@fullcalendar/timegrid';
-import dayGridPlugin from '@fullcalendar/daygrid';
-import interactionPlugin from '@fullcalendar/interaction';
+import timeGridPlugin from "@fullcalendar/timegrid";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import interactionPlugin from "@fullcalendar/interaction";
 
 function Calendar() {
-    return <div>
-        <Fullcalendar
-            plugins={[dayGridPlugin,timeGridPlugin, interactionPlugin]}
-            intialView={"dayGridMonth"}
-        />
-        </div>
+  return (
+    <div>
+      <Fullcalendar
+        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+        intialView={"dayGridMonth"}
+        headerToolbar={{
+            start: 'title', // will normally be on the left. if RTL, will be on the right
+            center: '',
+            end: 'today prev,next' // will normally be on the right. if RTL, will be on the left
+          }}
+          height={"90vh"}
+      />
+    </div>
+  );
 }
 
-export default Calendar; 
+export default Calendar;
--- src/app/whatson/page.js
+++ src/app/whatson/page.js
@@ -1,5 +1,5 @@
-import Calendar from '/components/calendar/calendar.jsx'
-import styles from "./whatson.module.css"
+import Calendar from "/components/calendar/calendar.jsx";
+import styles from "./whatson.module.css";
 
 export default function Whatson() {
   return (
