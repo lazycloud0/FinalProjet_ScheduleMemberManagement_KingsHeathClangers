181ab7ce58e6bb5157e3cda5842f28d74806dfbe
Kels <kelsey.baker1960@gmail.com>
2024-05-09 18:26:14 +0100

    organising code

--- components/header/header.jsx
+++ components/header/header.jsx
@@ -1,13 +1,21 @@
+//The header component contains the navigation bar for the website
+
 "use client";
 import React, { useState } from "react";
 import styles from "./header.module.css";
 import Smalllogo from "../logos/smallLogo.jsx";
 import Link from "next/link";
 
+//tThe header component
 export default function Header() {
+  //State to manage the menu open and close
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
+    //The ul contains the navigation links and the hamburger icon
+    //the navmenu uses state to determine if it should be active(in view) or not
+    //The hamburger icon uses the onClick event listener and state to determine if it should look like a burger or a cross
+    //When the hamburger icon is clicked, the state is toggled to show or hide the navigation links
     <header className={styles.header}>
       <nav className={styles.navbar}>
         <Smalllogo className={styles.navbranding} />
