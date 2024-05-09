b071438fa2b04a0b510edbd7a30c24f0d043ac7d
Kels <kelsey.baker1960@gmail.com>
2024-05-09 12:33:05 +0100

    organising code

--- components/calendar/calendar.jsx
+++ components/calendar/calendar.jsx
@@ -1,20 +1,28 @@
+// Pseudo Code: Calendar component to display the calendar view of the application
 "use client";
 import React from "react";
+//All of the dependencies that are required to use the Fullcalendar component
 import Fullcalendar from "@fullcalendar/react";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin from "@fullcalendar/interaction";
 
+//Calendar component that will display the calendar view of the application
 function Calendar() {
   return (
+      //Fullcalendar component that will display the calendar
+        //Plugins that are required to display the calendar (day, time and interaction)
+        //Initial view of the calendar will be dayGridMonth
+        //Header toolbar that will display the title, today and previous and next buttons
+          //Height of the calendar is set to 90vh (potentially change this through CSS modules)
     <div>
       <Fullcalendar
         plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
         intialView={"dayGridMonth"}
         headerToolbar={{
-            start: 'title', // will normally be on the left. if RTL, will be on the right
+            start: 'title', 
             center: '',
-            end: 'today prev,next' // will normally be on the right. if RTL, will be on the left
+            end: 'today prev,next'
           }}
           height={"90vh"}
       />
