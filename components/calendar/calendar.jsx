3d215d11a391352838d378b04fd49b548d11c4b8
Albert-S2 <albert733844@gmail.com>
2024-05-09 19:24:15 +0100

    footer moslty done

--- components/calendar/calendar.jsx
+++ components/calendar/calendar.jsx
@@ -12,11 +12,11 @@ function Calendar() {
         plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
         intialView={"dayGridMonth"}
         headerToolbar={{
-            start: 'title', // will normally be on the left. if RTL, will be on the right
-            center: '',
-            end: 'today prev,next' // will normally be on the right. if RTL, will be on the left
-          }}
-          height={"90vh"}
+          start: "title", // will normally be on the left. if RTL, will be on the right
+          center: "",
+          end: "today prev,next", // will normally be on the right. if RTL, will be on the left
+        }}
+        height={"90vh"}
       />
     </div>
   );
