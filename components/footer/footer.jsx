de2de8be8ded168b3f98ed2b5121c6dac4305458
Albert-S2 <albert733844@gmail.com>
2024-05-12 16:17:46 +0100

    footer font and  element sizes corrected in line with prototype

--- components/footer/footer.jsx
+++ components/footer/footer.jsx
@@ -10,26 +10,26 @@ export default function Footer() {
   return (
     <div className={styles.footerMenu}>
       <div className={styles.container}>
-      <div className={styles.footerIcons}>
-        <FacebookLogo className={styles.socialIcon} />
-        <YoutubeLogo className={styles.socialIconY} />
-        <InstagramLogo className={styles.socialIcon} />
-      </div>
-      <div className={styles.copyright}>
-        <h2>Kings Heath Clangers Community Basketball Club</h2>
-        <h5>
-          Copyright &copy; 2024 Kings Heath Clangers Community Basketball Club
-        </h5>
-      </div>
-      <div className={styles.legalStuff}>
-        <div>Legal Stuff</div>
-        
-        <div>Privacy Policy</div>
-       
-        <div>SiteMap</div>
-      
-        <div>Contact</div>
-      </div>
+        <div className={styles.footerIcons}>
+          <FacebookLogo className={styles.socialIcon} />
+          <YoutubeLogo className={styles.socialIconY} />
+          <InstagramLogo className={styles.socialIcon} />
+        </div>
+        <div className={styles.copyright}>
+          <h4>Kings Heath Clangers Community Basketball Club</h4>
+          <h6>
+            Copyright &copy; 2024 Kings Heath Clangers
+          </h6>
+        </div>
+        <div className={styles.legalStuff}>
+          <h6>Legal Stuff</h6>
+
+          <h6>Privacy Policy</h6>
+
+          <h6>SiteMap</h6>
+
+          <h6>Contact</h6>
+        </div>
       </div>
     </div>
   );
