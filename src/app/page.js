8e492bac80d9f92f1c2c58c3f3a40ab07121f0ba
Ayodimeji-stacks <149108938+Ayodimeji-stacks@users.noreply.github.com>
2024-05-14 10:12:10 +0100

    pages router set-up

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,13 @@
 import styles from "./page.module.css";
+import  {fetchGames}  from "./route.js";
+
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <p>Hello World</p>
+
+      <button onClick={fetchGames}>Fetch Games</button>
     </main>
   );
 }
