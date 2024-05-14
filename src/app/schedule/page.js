726532cf5e990cea9a1cc48fb3179c32ae4bba10
Albert-S2 <albert733844@gmail.com>
2024-05-14 17:05:56 +0100

    scedule form completed

--- src/app/schedule/page.js
+++ src/app/schedule/page.js
@@ -1,3 +1,15 @@
+import styles from "./schedule.module.css";
+import LargeLogo from "../../../components/logos/largeLogo";
+import Form from "/components/scheduleform/scheduleform.jsx";
+
+
 export default function Schedule() {
-  return <p>Hello from About page!</p>;
+  return (
+    <div className={styles.schedulePage}>
+      <div className={styles.container}>
+        <LargeLogo className={styles.largeLogo} />
+        <Form />
+      </div>
+    </div>
+  );
 }
