916a608c811d35a988025df7a56b53c8897fe4f7
Albert-S2 <albert733844@gmail.com>
2024-05-15 09:36:56 +0100

    dummy data added

--- components/calendar/calendar.jsx
+++ components/calendar/calendar.jsx
@@ -8,6 +8,17 @@ import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin from "@fullcalendar/interaction";
 import styles from "./calendar.module.css";
 
+const eventsPlaceholder = [
+  { title: 'Event 1', date: '2024-05-14' },
+  { title: 'Event 2', date: '2024-05-15' },
+  { title: 'Event 3', date: '2024-05-16' },
+  { title: 'Event 4', date: '2024-05-16' },
+  { title: 'Event 5', date: '2024-05-19' },
+  { title: 'Event 6', date: '2024-05-20' },
+  { title: 'Event 7', date: '2024-05-23' }
+];
+
+
 //Calendar component that will display the calendar view of the application
 function Calendar() {
   return (
@@ -26,6 +37,7 @@ function Calendar() {
           end: "today prev,next",
         }}
         aspectRatio={1.5}
+        events={eventsPlaceholder}
       />
     </div>
   );
