c0bb0f9a7cc1e88c83ef584856dfbf799370ff6c
Kels <kelsey.baker1960@gmail.com>
2024-05-18 16:13:36 +0100

    making progress

--- components/bookingform/bookingform.jsx
+++ components/bookingform/bookingform.jsx
@@ -3,25 +3,27 @@ import React, { useEffect, useState } from "react";
 import styles from "./bookingform.module.css";
 import { supabase } from "../../utils/supabase";
 
-
 export default function BookingForm() {
-    const [message, setMessage] = useState("");
-    const [dbData, setDbData] = useState({ field1: '', field2: '', field3: '', field4: '' });
-  
-     // Fetch data from the database
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('/api/your-endpoint'); // Replace with your actual API endpoint
-        const data = await response.json();
-        setDbData(data);
-      } catch (error) {
-        console.error('Error fetching data: ', error);
-      }
-    };
+  const [message, setMessage] = useState("");
+  const [dbData, setDbData] = useState({
+    field1: "",
+    field2: "",
+    field3: "",
+    field4: "",
+  });
 
-    fetchData();
-  }, []); // Empty dependency array means this effect runs once on component mount
+  // Define a `useEffect` hook that performs a get request from supabase and map the desired data out.
+  useEffect(() => {
+    const getGame = async () => {
+      const { data: games } = await supabase.from("games").select("*");
+      const eventData = games.map((game) => ({
+        title: game.event_type,
+        date: game.date,
+      }));
+      setEvents(eventData);
+    };
+    getGame();
+  }, []);
 
   function handleInputChanges(e) {
     const { name, value } = e.target;
@@ -31,20 +33,18 @@ export default function BookingForm() {
   return (
     <>
       <div className={styles.container}>
-        <form
-          className={styles.contactForm}
-        >
+        <form className={styles.contactForm}>
           <h3 className={styles.subtitle}>Book your place</h3>
           <div>{dbData.field1}</div>
-      <div>{dbData.field2}</div>
-      <div>{dbData.field3}</div>
-      <div>{dbData.field4}</div>
-      <input type="text" onChange={handleInputChanges} />
-          <button
-            className={styles.requestButton}
-            type="submit"
-          >
+          <div>{dbData.field2}</div>
+          <div>{dbData.field3}</div>
+          <div>{dbData.field4}</div>
+          <input type="text" onChange={handleInputChanges} />
+          <button className={styles.requestButton} type="submit">
             BOOK
+          </button>
+          <button className={styles.cancelButton} type="submit">
+            CANCEL
           </button>
         </form>
       </div>
