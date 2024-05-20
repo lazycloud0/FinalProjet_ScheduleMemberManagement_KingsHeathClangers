e899600f9038fa046b0f2b10d15ccc23cc131bcb
W <lazypink@gmail.com>
2024-05-20 14:40:54 +0100

    auth setup

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,15 +1,76 @@
-import React from "react";
-import LargeLogo from "../../../components/logos/largeLogo";
-import styles from "./login.module.css";
-import LoginForm from "/components/loginform/loginform.jsx";
+// import React from "react";
+// import LargeLogo from "../../../components/logos/largeLogo";
+// import styles from "./login.module.css";
+// import LoginForm from "/components/loginform/loginform.jsx";
 
-export default function Login() {
+// export default function Login() {
+//   return (
+//     <div className={styles.LoginPage}>
+//       <div className={styles.container}>
+//         <LargeLogo className={styles.largeLogo} />
+//         <LoginForm />
+//       </div>
+//     </div>
+//   );
+// }
+"use client";
+
+// import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+import { createClient } from "../../../utils/supabase.js";
+
+export default function LoginPage() {
+  const router = useRouter();
+  // const supabase = createClient();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function logIn() {
+    const { error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+    if (error) {
+      console.error(error);
+    }
+    router.push("/");
+  }
+
+  async function signUp() {
+    const { error } = await supabase.auth.signUp({ email, password });
+    if (error) {
+      console.error(error);
+    }
+    router.push("/");
+  }
+
   return (
-    <div className={styles.LoginPage}>
-      <div className={styles.container}>
-        <LargeLogo className={styles.largeLogo} />
-        <LoginForm />
-      </div>
-    </div>
+    <main>
+      <form>
+        <label htmlFor="email">Email:</label>
+        <input
+          id="email"
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label htmlFor="password">Password:</label>
+        <input
+          id="password"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="button" onClick={logIn}>
+          Log in
+        </button>
+        <button type="button" onClick={signUp}>
+          Sign up
+        </button>
+      </form>
+    </main>
   );
 }
