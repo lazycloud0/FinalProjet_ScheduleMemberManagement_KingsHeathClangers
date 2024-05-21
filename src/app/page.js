e94b088cb634bce69f728eac596eb5dac7153cca
Kels <kelsey.baker1960@gmail.com>
2024-05-21 11:02:51 +0100

    landing page styling done

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,16 @@
 import LargeLogo from "../../components/logos/largeLogo";
 import styles from "./page.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <LargeLogo className={styles.largeLogo} />
-      <button className={styles.loginButton}>LOGIN</button>
+      <button className={styles.loginButton}>
+        <Link href="/login" className={styles.linkDecoration}>
+          LOGIN
+        </Link>
+      </button>
     </main>
   );
 }
