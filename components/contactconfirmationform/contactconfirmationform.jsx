7c33b99c0c8eaa8f6726240d438cba443c626fc7
Albert-S2 <albert733844@gmail.com>
2024-05-22 11:01:45 +0100

    contact form updated

---
+++ components/contactconfirmationform/contactconfirmationform.jsx
@@ -0,0 +1,26 @@
+"use client";
+import React from "react";
+import { useSearchParams } from "next/navigation";
+import { useRouter } from "next/navigation"; // Import useRouter correctly
+import styles from "./contactconfirmationform.module.css"; // Import your CSS module
+
+export default function EditConfirmation() {
+  const searchParams = useSearchParams();
+  const router = useRouter(); // Initialize useRouter
+  const message = searchParams.get("message");
+
+  const handleReturn = () => {
+    router.push("/"); // Redirect to /whatson page
+  };
+
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.successMessage}>MESSAGE SENT SUCCESSFULY</h1>
+      <h3>Thank you for contacting Kings Heath Clangers</h3>
+      <button className={styles.returnButton} onClick={handleReturn}>
+        RETURN TO HOME PAGE
+      </button>{" "}
+      {/* Add button */}
+    </div>
+  );
+}
