9d928feb4a2c65241abc8c6e1af26e1c9be95e08
Kels <kelsey.baker1960@gmail.com>
2024-05-24 09:37:20 +0100

    about page mobile site

---
+++ components/aboutpage/aboutpage.jsx
@@ -0,0 +1,82 @@
+"use client";
+import React from "react";
+import styles from "./aboutpage.module.css";
+
+export default function AboutPage() {
+  return (
+    <div className={styles.aboutPage}>
+      <section className={styles.about}>
+        <h1 className={styles.title}>Who We Are</h1>
+        <p className={styles.text}>
+          Established in Kings Heath Park in 2020 and formalised into a Club in
+          2024, Kings Heath Community Basketball is a collective built on{" "}
+          <span className={styles.singleWord}>grit</span>,
+          <span className={styles.singleWord}> determination</span> and a desire
+          to <span className={styles.singleWord}>win</span> with{" "}
+          <span className={styles.singleWord}>integrity</span> in both sport and
+          life.
+        </p>
+      </section>
+      <img
+        src="/teamphoto.jpg"
+        alt="A team photo of Kings Heath Community Basketball Club"
+        className={styles.teamPhoto}
+      />
+      <section className={styles.mission}>
+        <h1 className={styles.title}>Mission</h1>
+        <p className={styles.text}>
+          Our primary goal is to establish a{" "}
+          <span className={styles.singleWord}>strong</span> and{" "}
+          <span className={styles.singleWord}>inclusive</span> community
+          basketball club with multiple teams across varied age groups, genders,
+          and from multiple socio-economic backgrounds. <br /> <br /> By
+          fostering a <span className={styles.singleWord}>positive </span>
+          and <span className={styles.singleWord}>supportive</span> environment,
+          we aim to promote the sport of basketball and its numerous benefits to
+          both players and the local community.
+        </p>
+      </section>
+      <section className={styles.philosophy}>
+        <h1 className={styles.title}>Philosophy</h1>
+        <p className={styles.text}>
+          <span className={styles.singleWord}>Ubuntu</span>, meaning I am
+          because we are, is the philosphy that provides a powerful,
+          community-orientated value for all our members to follow
+        </p>
+      </section>
+      <video src="/khcvideo.mp4" controls className={styles.video}></video>
+      <section className={styles.leagues}>
+        <h1 className={styles.title}>Achievements</h1>
+        <p className={styles.text}>
+          Currently{" "}
+          <span className={styles.singleWord}>Division 3 Championships</span> in
+          West Midlands Basketball league and current holders of the{" "}
+          <span className={styles.singleWord}>Babe Clay Rosebowl Cup</span>, we
+          have exceeded expectations in our first season and have ambitious
+          goals for our Youth, {"Women's"} and {"Men's"} teams.
+        </p>
+      </section>
+      <section className={styles.images}>
+        <p className={styles.carousel}>An Image Carousel will go here</p>
+      </section>
+      <section className={styles.timetable}>
+        <h1 className={styles.title}>Timetable</h1>
+        <h3 className={styles.timeTableSubtitle}>Mondays:</h3>
+        <p className={styles.groups}>Open Scrimmage:</p>
+        <p>18:00 (18+ Only)</p>
+        <h3 className={styles.timeTableSubtitle}>Saturdays:</h3>
+        <p className={styles.groups}>Under {"15's"} Training:</p>{" "}
+        <p className={styles.time}>12:00-13:30</p>
+        <p className={styles.groups}>Under {"18's"} Training:</p>{" "}
+        <p className={styles.time}>13:30-15:00</p>
+        <p className={styles.groups}>{"Women's"} Training:</p>
+        <p className={styles.time}>12:00-13:00 (Female Coach)</p>
+        <p className={styles.groups}>{"Men's"} Training:</p>
+        <p className={styles.time}>13:00-15:00 (Members Only)</p>
+        <h3 className={styles.timeTableSubtitle}>Sundays:</h3>
+        <p className={styles.groups}>Games:</p>
+        <p className={styles.lastTime}>18:00</p>
+      </section>
+    </div>
+  );
+}
--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,3 +1,11 @@
+import React from "react";
+import styles from "./about.module.css";
+import AboutPage from "/components/aboutpage/aboutpage.jsx";
+
 export default function About() {
-  return <p>Hello from About page!</p>;
+  return (
+    <div className={styles.aboutPage}>
+      <AboutPage />
+    </div>
+  );
 }
